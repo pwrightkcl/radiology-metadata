de42a7a64115d99572f573a6dd71b488fb6b0896
Paul Wright <p.wright@kcl.ac.uk>
2026-03-17 11:47:01 +0000

    Tidy logging. Change output to project_dir.

--- BIDS/pick_dicom.py
+++ BIDS/pick_dicom.py
@@ -35,16 +35,26 @@ def _invalidate(di: pd.DataFrame, invalid: pd.Series, reason: str) -> None:
         print(f"Invalidated {pre - post} records: {reason}. {post} records remain.")
 
 
-def main(input_index: Path, output_index: Path):
+def main(input_index: Path, project_dir: Path):
     """Apply heuristics to impute BIDS entities from DICOM attributes and pick images for a specific project.
+
+    Saves a new DICOM index with additional BIDS entity columns (at least `suffix`) and selection columns:
+
+    - `valid`: whether the record was included
+    - `reason`: "valid" or reason for exclusion
+
+    The output index will be saved to `{project_dir}/metadata/dicom_index_picks` as CSV and parquet.
 
     Args:
         input_index: Path to the input DICOM index file (parquet format).
-        output_index: Path to save the output DICOM index file (parquet format).
+        project_dir: Path to the project directory, where output index will be saved.
     
     Returns:
         None. Saves the processed DICOM index to the specified output path.
     """
+    if not project_dir.is_dir():
+        raise FileNotFoundError(f"Project directory not found: {project_dir}")
+    
     print("Loading data")
     di = pd.read_parquet(input_index)
     print(f"Loaded {len(di)} records from {input_index}.")
@@ -57,10 +67,10 @@ def main(input_index: Path, output_index: Path):
     di['reason'] = "Valid"
 
     # Only include modality MR
-    _invalidate(di, di['Modality'] != 'MR', 'Not MR.')
+    _invalidate(di, di['Modality'] != 'MR', 'Not MR')
 
     # Exclude images with blank SeriesDescription
-    _invalidate(di, di['SeriesDescription'].isnull(), "No SeriesDescription.")
+    _invalidate(di, di['SeriesDescription'].isnull(), "No SeriesDescription")
 
     print(f"Found {di['valid'].sum()} valid records.")
 
@@ -108,19 +118,20 @@ def main(input_index: Path, output_index: Path):
     apply_heuristics(di, heuristics)
 
     # Apply project-specific criteria
-    _invalidate(di, ~di['suffix'].isin(['T1w', 'FLAIR', 'dwi']), "Not T1, FLAIR, or DWI.")
+    _invalidate(di, ~di['suffix'].isin(['T1w', 'FLAIR', 'dwi']), "Not T1, FLAIR, or DWI")
 
     print("Final counts:")
     print(di['reason'].value_counts(dropna=False))
 
     print('Saving')
+    output_index = project_dir / 'metadata' / 'dicom_index_picks'
     di.to_csv(output_index.with_suffix('.csv'), index=False, quoting=csv.QUOTE_NONNUMERIC)
     di.to_parquet(output_index.with_suffix('.parquet'))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Apply heuristics to impute BIDS entities from DICOM attributes and pick images for a specific project.")
-    parser.add_argument("input_index", type=Path, help="Path to the input DICOM index file (parquet format).")
-    parser.add_argument("output_index", type=Path, help="Path to save the output DICOM index file (parquet format).")
+    parser.add_argument("--input_index", type=Path, help="Path to the input DICOM index file (parquet format).")
+    parser.add_argument("--project_directory", type=Path, help="Path to the project directory, where output index will be saved.")
     args = parser.parse_args()
-    main(args.input_index, args.output_index)
+    main(args.input_index, args.project_directory)
